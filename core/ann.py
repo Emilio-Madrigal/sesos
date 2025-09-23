b1a1ba9368e3d9ec7683cb5b9f006b7a62579223
Emilio <madrigalemi31@gmail.com>
2025-09-22 19:27:20 -0600

    ya estoy cansado solo falta el brain.py pero no quiero seguir

--- core/ann.py
+++ core/ann.py
@@ -1,30 +1,41 @@
-import numpy as np
+import tensorflow as tf
 from .layer import Layer
 
 class ANN:
-    def __init__(self,estructura):
+    def __init__(self, estructura, activaciones=None):
         self.layers = []
-        
+        self.loss_list = []
+
+        if activaciones is None:
+            activaciones = ['sigmoide'] * (len(estructura) - 1)
+
         for i in range(1, len(estructura)):
             cantidad_entradas = estructura[i - 1]
             cantidad_neuronas = estructura[i]
-            self.layers.append(Layer(cantidad_neuronas, cantidad_entradas))
+            activacion = activaciones[i - 1]
+            self.layers.append(Layer(cantidad_entradas, cantidad_neuronas, activacion))
 
-        self.loss_list = []
-
-    def add_layer(self, cantidad_entradas, cantidad_neuronas):
-
-        if not self.layers:
-            self.layers.append(Layer(cantidad_entradas, cantidad_neuronas))
-        else:
-            cantidad_entradas_anterior=len(self.layer[-1].neuronas)
-            self.layers.append(Layer(cantidad_entradas_anterior, cantidad_neuronas))
+    @tf.function
     def forward(self, entradas):
-
+        salida=entradas
         for layer in self.layers:
-            entradas=layer.forward(entradas)
-            return entradas
+            salida=layer.forward(salida)
+        return salida
     
     def backward(self, loss_gradient, tasa_aprendizaje):
+        gradiente=loss_gradient
         for layer in reversed(self.layers):
-            loss_gradient=layer.backward(loss_gradient,tasa_aprendizaje)
+            gradiente=layer.backward(gradiente,tasa_aprendizaje)
+        return gradiente
+    
+    def get_trainable_variables(self):
+        variables = []
+        for layer in self.layers:
+            variables.extend(layer.get_trainable_variables())
+        return variables
+    
+    def compilar_con_optimizador(self, optimizador='adam', tasa_aprendizaje=0.01):
+        if optimizador=='adam':
+            self.optimizer = tf.keras.optimizers.Adam(tasa_aprendizaje)
+        elif optimizador=='sgd':
+            self.optimizador=tf.keras.optimizers.SGD(tasa_aprendizaje)
